203e8af03e8c1816cc3350ade083bf33ea91d409
Martinovics <balazsmartin0812@gmail.com>
2022-11-17 14:33:09 +0100

    basic fragments

---
+++ app/src/main/java/hu/bp/mrtn/workoutdesigner/fragments/WorkoutsFragment.kt
@@ -0,0 +1,30 @@
+package hu.bp.mrtn.workoutdesigner.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hu.bp.mrtn.workoutdesigner.R
+
+
+class WorkoutsFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workouts, container, false)
+    }
+
+
+}
